8632b89459af7bd44165c5ff14c19e695e4f71aa
Raissa Arantes <rayssa.hcl@gmail.com>
2021-04-20 20:25:32 -0300

    Consumindo API na Home usando SSR e SSG

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,7 +1,28 @@
+import { useEffect } from "react"
+import { Player } from "../components/Player"
 
-export default function Home() {
+
+export default function Home(props) {
+  console.log(props.episodes)
   return (
-    <h1>Index</h1>
-
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
   )
+}
+
+//Com SSR - busca os dados msm com Js Disable no Client side (Browser)
+//SSG evita a constante requisição toda vez que um user acessa a home
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8 //a cada 8h
+  }
 }
